1d4da6dbcc3830e421bc725424e3a6f387df670e
ShivaniJadhav <97583560+ShivuJ@users.noreply.github.com>
2024-10-09 00:41:12 +0530

    User Add Update issue

--- src/main/resources/static/js/teacher.js
+++ src/main/resources/static/js/teacher.js
@@ -1,137 +1,139 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	console.log("Document is Ready....");
-	 /*Toggle model for form */
+	/*Toggle model for form */
 	document.getElementById('toggleFormBtn').addEventListener('click', function() {
 		toggleUserBtn();
 	});
-			
-	function toggleUserBtn(){
+
+	function toggleUserBtn() {
 		const formContainer = document.getElementById('formContainer');
-            if (formContainer.style.display === 'none' || formContainer.style.display === '') {
-                formContainer.style.display = 'block';
-            } else {
-                formContainer.style.display = 'none';
-            }
+		if (formContainer.style.display === 'none' || formContainer.style.display === '') {
+			formContainer.style.display = 'block';
+		} else {
+			formContainer.style.display = 'none';
+		}
 	}
-	/* POST reuest to add data*/	
-	$('#submit').click(function(){
-		addTeacher();	
+	/* POST reuest to add data*/
+	$('#submit').click(function() {
+		addTeacher();
 	});
-	
-	$(".userTable").on('click', '#deleteBtn',function(){
+
+	$(".userTable").on('click', '#deleteBtn', function() {
 		var id = $(this).closest('tr').data("id");
 		deleteUser(id);
 	});
-	
-	$(".userTable").on('click', '#editBtn',function(){
+
+	$(".userTable").on('click', '#editBtn', function() {
 		$('#submit').text("Update");
 		toggleUserBtn();
 		var id = $(this).closest('tr').data("id");
-		if(id){
+		if (id) {
 			editUser(id);
-		}else {
+		} else {
 			toastr.error("Id is null");
 		}
-		
+
 	});
-	
-	function getUserData(){
-		return 	{
-					id: $('#userId').val(),
-					firstName : $('#firstName').val(),
-					lastName : $('#lastName').val(),
-					email : $('#teacherEmail').val(),
-					teacherClass : $('#teachingClass').val(),
-					subject : $('#subject').val(),
-					dateOfJoining : $('#dateOfJoining').val(),
-					employmentStatus : $('#employmentStatus').val(),
-					qualification : $('#qualification').val(),
-					yearOfGraduation : $('#yearOfGraduation').val(),
-					phone : $('#contact').val(),
-					password : $('#password').val(),
-					role : $('#role').val(),
-					status : "Y"
-				}
+
+	function getUserData() {
+		return {
+			id: $('#userId').val(),
+			firstName: $('#firstName').val(),
+			lastName: $('#lastName').val(),
+			email: $('#teacherEmail').val(),
+			teacherClass: $('#teachingClass').val(),
+			subject: $('#subject').val(),
+			dateOfJoining: $('#dateOfJoining').val(),
+			employmentStatus: $('#employmentStatus').val(),
+			qualification: $('#qualification').val(),
+			yearOfGraduation: $('#yearOfGraduation').val(),
+			phone: $('#contact').val(),
+			password: $('#password').val(),
+			role: $('#role').val(),
+			status: "Y"
+		}
 	}
-	
-	function isValidate(saveData){
-		if(!saveData.firstName){
-           toastr.error("Please add first name");
-           return false;
-       } else if(!saveData.lastName){
-           toastr.error("Please add last name");
-           return false;
-       } else if(!saveData.email){
-           toastr.error("Please add email id");
-           return false;
-       } else if(!saveData.role){
-           toastr.error("Please select role");
-           return false;
-       } else if(!saveData.teacherClass){
-           toastr.error("Please select class");
-           return false;
-       } else if(!saveData.phone || saveData.phone.length != 10){
-           toastr.error("Please add a proper contact number");
-           return false;
-       } else if(!saveData.subject){
-           toastr.error("Please select subject");
-           return false;
-       } else if(!saveData.dateOfJoining){
-           toastr.error("Please select Date of Joining");
-           return false;
-       } else if(!saveData.employmentStatus){
-           toastr.error("Please select employment status");
-           return false;
-       } else if(!saveData.qualification){
-           toastr.error("Please select qualification");
-           return false;
-       } else if(!saveData.yearOfGraduation){
-           toastr.error("Please add year of graduation");
-           return false;
-       } else if(!saveData.password || saveData.password.length < 8){
-           toastr.error("Password should be of at least 8 characters");
-           return false;
-       }
-       return true;
+
+	function isValidate(saveData) {
+		if (!saveData.firstName) {
+			toastr.error("Please add first name");
+			return false;
+		} else if (!saveData.lastName) {
+			toastr.error("Please add last name");
+			return false;
+		} else if (!saveData.email) {
+			toastr.error("Please add email id");
+			return false;
+		} else if (!saveData.role) {
+			toastr.error("Please select role");
+			return false;
+		} else if (!saveData.teacherClass) {
+			toastr.error("Please select class");
+			return false;
+		} else if (!saveData.phone || saveData.phone.length != 10) {
+			toastr.error("Please add a proper contact number");
+			return false;
+		} else if (!saveData.subject) {
+			toastr.error("Please select subject");
+			return false;
+		} else if (!saveData.dateOfJoining) {
+			toastr.error("Please select Date of Joining");
+			return false;
+		} else if (!saveData.employmentStatus) {
+			toastr.error("Please select employment status");
+			return false;
+		} else if (!saveData.qualification) {
+			toastr.error("Please select qualification");
+			return false;
+		} else if (!saveData.yearOfGraduation) {
+			toastr.error("Please add year of graduation");
+			return false;
+		} else if (!saveData.password || saveData.password.length < 8) {
+			toastr.error("Password should be of at least 8 characters");
+			return false;
+		}
+		return true;
 	}
-		
-	function addTeacher(){
+
+	function addTeacher() {
 		var saveData = getUserData();
 		console.log(saveData);
-		if(isValidate(saveData)){
+		if (isValidate(saveData)) {
 			$.ajax({
-				url : '/addTeacher',
-				type : 'POST',
-				contentType : 'application/json',
-				data : JSON.stringify(saveData),
-				dataType : 'JSON',
-				success : function(response){
-					if(response.status == 200){
+				url: '/addTeacher',
+				type: 'POST',
+				contentType: 'application/json',
+				data: JSON.stringify(saveData),
+				dataType: 'JSON',
+				success: function(response) {
+					if (response.status == 200) {
 						toastr.success('Data Save Successfully');
-						location.reload();
-					}else {
+						setTimeout(function() {
+							location.reload();
+						}, 2000);
+					} else {
 						toastr.error('Something went wrong');
 					}
-				},			
+				},
 				error: function(xhr, status, error) {
-				           // alertify.error("An error occurred: " + error);
-				        }
+					// alertify.error("An error occurred: " + error);
+				}
 
-			});					
+			});
 		}
-		
+
 	}
-	
-	/* Get reuest to view data*/	
-	$(window).on('load', function(){
+
+	/* Get reuest to view data*/
+	$(window).on('load', function() {
 		console.log("Getting Table Ready")
 		$.ajax({
 			url: 'teacher',
 			type: 'GET',
-			success: function(response){
+			success: function(response) {
 				let html = "";
-				for(let i = 0; i < response.length; i++){
-					if(response[i].status == "Y"){
+				for (let i = 0; i < response.length; i++) {
+					if (response[i].status == "Y") {
 						html += `
 							<tr data-id = "${response[i].id}">
 								<td>${i + 1}</td>
@@ -156,31 +158,31 @@ $(document).ready(function(){
 			}
 		})
 	});
-	
+
 	/*Delete User */
-	function deleteUser(id){
-			$.ajax({
-				url: "/deleteUser",
-				type : 'POST',
-				contentType : 'application/json',
-				data : JSON.stringify(id),
-				dataType : 'JSON',
-				success : function(response){
-					if(response==="Success"){
-						toastr.success("User Deleted Successfully.");
-					}else{
-						toastr.error("Something went wrong.");
-					}
+	function deleteUser(id) {
+		$.ajax({
+			url: "/deleteUser",
+			type: 'POST',
+			contentType: 'application/json',
+			data: JSON.stringify(id),
+			dataType: 'JSON',
+			success: function(response) {
+				if (response === "Success") {
+					toastr.success("User Deleted Successfully.");
+				} else {
+					toastr.error("Something went wrong.");
 				}
-					
-			});
-		}
-		
-	function editUser(id){
+			}
+
+		});
+	}
+
+	function editUser(id) {
 		$.ajax({
 			url: '/editUser/' + id,
 			type: 'GET',
-			success: function(response){
+			success: function(response) {
 				console.log(response);
 				$('#userId').val(response.id);
 				$('#firstName').val(response.firstName);
@@ -198,7 +200,7 @@ $(document).ready(function(){
 			}
 		});
 	}
-		
+
 	/*function updateUser(id){
 		var saveData = getUserData();
 				console.log(saveData);
@@ -217,8 +219,8 @@ $(document).ready(function(){
 							}
 						},			
 						error: function(xhr, status, error) {
-						           // alertify.error("An error occurred: " + error);
-						        }
+								   // alertify.error("An error occurred: " + error);
+								}
 
 					});					
 				}
