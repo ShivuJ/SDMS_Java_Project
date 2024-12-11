05b632a8c9321091dfe673f797bf841adedd1247
ShivaniJadhav <97583560+ShivuJ@users.noreply.github.com>
2024-12-12 00:20:45 +0530

    WIP - Subject database structure created

---
+++ src/main/java/com/sdmsproject/sdms/ServiceImpl/SubjectServiceImpl.java
@@ -0,0 +1,10 @@
+package com.sdmsproject.sdms.ServiceImpl;
+
+import org.springframework.stereotype.Service;
+
+import com.sdmsproject.sdms.Service.SubjectService;
+
+@Service
+public class SubjectServiceImpl implements SubjectService {
+
+}
---
+++ src/main/java/com/sdmsproject/sdms/model/SubjectEntity.java
@@ -0,0 +1,82 @@
+package com.sdmsproject.sdms.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "subject_db")
+public class SubjectEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String subject;
+	private char status = 'Y';
+	private String createdBy;
+	private String createdOn;
+	private String updatedBy;
+	private String updatedOn;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public char getStatus() {
+		return status;
+	}
+
+	public void setStatus(char status) {
+		this.status = status;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public String getCreatedOn() {
+		return createdOn;
+	}
+
+	public void setCreatedOn(String createdOn) {
+		this.createdOn = createdOn;
+	}
+
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	public String getUpdatedOn() {
+		return updatedOn;
+	}
+
+	public void setUpdatedOn(String updatedOn) {
+		this.updatedOn = updatedOn;
+	}
+
+}
