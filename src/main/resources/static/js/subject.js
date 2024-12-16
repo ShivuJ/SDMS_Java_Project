7d1054fc52a7678eed2a414311733da63f9af318
ShivaniJadhav <97583560+ShivuJ@users.noreply.github.com>
2024-12-17 00:56:38 +0530

    WIP - Create database tables for managing subjects

--- src/main/resources/static/js/subject.js
+++ src/main/resources/static/js/subject.js
@@ -1,17 +1,55 @@
 $(document).ready(function() {
 	document.getElementById('toggleFormBtn').addEventListener('click', function() {
-			toggleUserBtn();
+		toggleUserBtn();
+	});
+
+	$('#navbar').load('nav.html');
+
+	function toggleUserBtn() {
+		const formContainer = document.getElementById('formContainer');
+		if (formContainer.style.display === 'none' || formContainer.style.display === '') {
+			formContainer.style.display = 'block';
+		} else {
+			formContainer.style.display = 'none';
+		}
+	}
+	
+	$('#submit').click(function() {
+			addSubject();
 		});
 
-		$('#navbar').load('nav.html');s
-
-		function toggleUserBtn() {
-			const formContainer = document.getElementById('formContainer');
-			if (formContainer.style.display === 'none' || formContainer.style.display === '') {
-				formContainer.style.display = 'block';
-			} else {
-				formContainer.style.display = 'none';
-			}
+	function getSubjectData() {
+		return {
+			id: $('#subjectId').val(),
+			subject: $('#subject').val()
 		}
-	
+	}
+
+	function isValidate(savedata) {
+		if (!savedata.subject) {
+			toastr.error("Please add subject");
+			return false;
+		}
+	}
+
+	function addSubject() {
+		var savedata = getSubjectData();
+		if (isValidate(savedata)) {
+			$.ajax({
+				url: '/addSubject',
+				type: 'POST',
+				contentType: 'application/json',
+				data: JSON.stringify(saveData),
+				dataType: 'JSON',
+				success: function(response) {
+					console.log(response);
+					if (response.status == "Y") {
+						toastr.success("User Added Successfully...");
+					} else {
+						toastr.error("Something Went Wrong...");
+					}
+				}
+			});
+		}
+	}
 });
